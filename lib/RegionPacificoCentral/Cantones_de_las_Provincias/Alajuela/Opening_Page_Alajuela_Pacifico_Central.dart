2a664d7215451cdd39ba9910d02d13043b9b718f
DekuCool24 <dayanna.qs@gmail.com>
2020-08-02 02:19:30 -0600

    Create Opening_Page_Alajuela_Pacifico_Central.dart

---
+++ lib/RegionPacificoCentral/Cantones_de_las_Provincias/Alajuela/Opening_Page_Alajuela_Pacifico_Central.dart
@@ -0,0 +1,52 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class regionSocialEconomicaPacificoCentralAlajuela extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Cantones de la Provincia de Alajuela',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('San Mateo', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Orotina', style: TextStyle(fontSize: 25),
+            ),
+          ),
+
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Gráfico de la Provincia', style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}

e9c247f1266b37370b6583335aa594075d6949e9
DekuCool24 <dayanna.qs@gmail.com>
2020-08-02 02:16:09 -0600

    Create Heredia_central_opening_page.dart

---
+++ lib/RegionCentral/Cantones_de_las_Provincias/Heredia/Heredia_central_opening_page.dart
@@ -0,0 +1,86 @@
+///Opening_Page
+///
+///This page contains the main page of the project, what
+///it contains are buttons with which you can choose which
+///graphics to observe
+///
+
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class regionSocialEconomicaCentralHeredia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center (
+              child: Text(
+                'Cantones de la Provincia de Heredia',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.teal,),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.map),
+            title: Text('Barba', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Belén', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Flores', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Heredia', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Isidro', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Pablo', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('San Rafael', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Santa Bárbara', style: TextStyle(fontSize: 25),
+            ),
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Santo Domingo', style: TextStyle(fontSize: 25),
+            ),
+          ),
+
+          ListTile(
+            leading: Icon(FontAwesomeIcons.trello),
+            title: Text('Gráfico de la Provincia', style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  }
+}

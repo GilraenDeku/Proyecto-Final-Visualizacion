ce6291077b6f56ef37f5de313e14a08be95309a3
DekuCool24 <dayanna.qs@gmail.com>
2020-08-02 02:19:19 -0600

    Create storytelling_region_Huetar_Atlantica.dart

---
+++ lib/RegionHuetarAtlantica/storytelling_region_Huetar_Atlantica.dart
@@ -0,0 +1,430 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class regionHuetarAtlanticaStorytelling extends StatelessWidget {
+  List<charts.Series> seriesList;
+  List<charts.Series> otherSeriesList;
+  final bool animate;
+
+  regionHuetarAtlanticaStorytelling(this.seriesList, this.otherSeriesList, {this.animate});
+
+  /// Creates a [BarChart] with sample data and no transition.
+  factory regionHuetarAtlanticaStorytelling.withSampleData() {
+    return new regionHuetarAtlanticaStorytelling(
+      _createSampleData(),
+      _createOtherSampleData(),
+      animate: true,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Center(
+                    child: Text(
+                      'Región Socioeconómica Huetar Atlántica',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Center(
+                    child: Text(
+                      'Regiones Socioeconómicas de Costa Rica',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+    */
+          //Text('1'),
+        ],
+      ),
+    );
+
+    Widget breveResenaSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Breve Reseña Histórica',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                    'La Región Huetar Atlántica es una región socioeconómica '
+                        'en la costa caribeña de Costa Rica. Abarca en su '
+                        'totalidad la provincia de Limón, y un distrito del '
+                        'Sarapiquí, en la provincia de Heredia. Incluye los '
+                        'cantones de la provincia de Limón: Limón, Pococí, '
+                        'Siquirres, Talamanca, Matina y Guácimo. De la '
+                        'provincia de Heredia: Distrito Horquetas del Cantón '
+                        'de Sarapiquí.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.find_in_page, 'WEB'),
+        ],
+      ),
+    );
+
+    Widget chartSection = Container(
+      height: 400,
+      width: 400,
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Promedio por vivienda de servicios de telefonía residencial',
+              style: TextStyle(
+                  fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Expanded(
+              child: new charts.PieChart(seriesList,
+                  animate: animate,
+                  // Configure the width of the pie slices to 60px. The remaining space in
+                  // the chart will be left as a hole in the center.
+                  //
+                  // [ArcLabelDecorator] will automatically position the label inside the
+                  // arc if the label will fit. If the label will not fit, it will draw
+                  // outside of the arc with a leader line. Labels can always display
+                  // inside or outside using [LabelPosition].
+                  //
+                  // Text style for inside / outside can be controlled independently by
+                  // setting [insideLabelStyleSpec] and [outsideLabelStyleSpec].
+                  //
+                  // Example configuring different styles for inside/outside:
+                  //       new charts.ArcLabelDecorator(
+                  //          insideLabelStyleSpec: new charts.TextStyleSpec(...),
+                  //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
+                  defaultRenderer: new charts.ArcRendererConfig(
+                      arcWidth: 60,
+                      arcRendererDecorators: [new charts.ArcLabelDecorator()])),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Promedio por vivienda de servicios de telefonía Celular',
+              style: TextStyle(
+                  fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Expanded(
+              child: new charts.PieChart(otherSeriesList,
+                  animate: animate,
+                  // Configure the width of the pie slices to 60px. The remaining space in
+                  // the chart will be left as a hole in the center.
+                  //
+                  // [ArcLabelDecorator] will automatically position the label inside the
+                  // arc if the label will fit. If the label will not fit, it will draw
+                  // outside of the arc with a leader line. Labels can always display
+                  // inside or outside using [LabelPosition].
+                  //
+                  // Text style for inside / outside can be controlled independently by
+                  // setting [insideLabelStyleSpec] and [outsideLabelStyleSpec].
+                  //
+                  // Example configuring different styles for inside/outside:
+                  //       new charts.ArcLabelDecorator(
+                  //          insideLabelStyleSpec: new charts.TextStyleSpec(...),
+                  //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
+                  defaultRenderer: new charts.ArcRendererConfig(
+                      arcWidth: 60,
+                      arcRendererDecorators: [new charts.ArcLabelDecorator()])),
+            ),
+          ],
+        ),
+      ),
+    );
+
+
+    Widget interpretacionCodigo= Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Interpretación del Gráfico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                    'Las regiones socioeconómicas de Costa Rica '
+                        '(a menudo denominadas sólo como regiones funcionales) '
+                        'son una subdivisión político-económica en la que '
+                        'se ha delimitado este país centroamericano. Esta subdivisión '
+                        'fue realizada por Decreto Ejecutivo Nº 7944 '
+                        'del 26 de enero de 1978.',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+                SizedBox(height: 20),
+                Text(
+                    'Estas regiones son seis en total: Región Central, '
+                        'Región Chorotega, Región Pacífico Central, '
+                        'Región Brunca, Región Huetar Atlántica y '
+                        'Región Huetar Norte. Algunos nombres de '
+                        'región se derivan de las etnias precolombinas '
+                        'que habitaron en esas zonas geográficas. ',
+                    softWrap: true,
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.justify),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+
+
+    return MaterialApp(
+        home: DefaultTabController(
+            length: 1,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Color(0xff1976d2),
+                title: Text('Región Huetar Atlántica'),
+              ),
+              body: ListView(
+                children: [
+                  Image.asset(
+                    'assets/regionHuetarCaribe.png',
+                    width: 600,
+                    height: 400,
+                    fit: BoxFit.cover,
+                  ),
+                  titleSection,
+                  buttonSection,
+                  breveResenaSection,
+                  chartSection,
+
+                  /*
+                  FlatButton(
+                    onPressed: () {
+                      showGeneralDialog(
+                          context: context,
+                          barrierDismissible: true,
+                          barrierLabel: MaterialLocalizations.of(context)
+                              .modalBarrierDismissLabel,
+                          barrierColor: Colors.black45,
+                          transitionDuration: const Duration(milliseconds: 500),
+                          pageBuilder: (BuildContext buildContext,
+                              Animation animation,
+                              Animation secondaryAnimation) {
+                            return Center(
+                              child: Container(
+                                width: MediaQuery.of(context).size.width-30,
+                                height: MediaQuery.of(context).size.height-500,
+                                padding: EdgeInsets.all(20),
+                                color: Colors.white,
+                                child: Column(
+                                  children: [
+                                    Image.asset(
+                                      'assets/regionChorotegaGrafico.jpg',
+                                      width: 1100,
+                                      height: 326,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                    textColor: Colors.black,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFFD16BA5),
+                            Color(0xFF86A8E7),
+                            Color(0xFF5FFBF1),
+                          ],
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(10.0),
+                      child:
+                      const Text('Ver Información del Gráfico', style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+
+                  */
+                  interpretacionCodigo,
+
+
+                ],
+              ),
+            )));
+  }
+
+
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
+    final data = [
+      new LinearSales(2010, 12.74),
+      new LinearSales(2011, 12.62),
+      new LinearSales(2012, 12.5),
+      new LinearSales(2013, 12.87),
+      new LinearSales(2014, 12.37),
+      new LinearSales(2015, 12.25),
+      new LinearSales(2016, 12.37),
+      new LinearSales(2017, 12.25),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Sales',
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        colorFn: (_, __) => charts.MaterialPalette.deepOrange.shadeDefault,
+        data: data,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (LinearSales row, _) => '${row.year}: ${row.sales}',
+      )
+    ];
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createOtherSampleData() {
+    final data = [
+      new LinearSales(2010, 09.10),
+      new LinearSales(2011, 10.80),
+      new LinearSales(2012, 12.92),
+      new LinearSales(2013, 13.68),
+      new LinearSales(2014, 13.56),
+      new LinearSales(2015, 13.45),
+      new LinearSales(2016, 13.04),
+      new LinearSales(2017, 13.45),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Sales',
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        colorFn: (_, __) => charts.MaterialPalette.deepOrange.shadeDefault,
+        data: data,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (LinearSales row, _) => '${row.year}: ${row.sales}',
+      )
+    ];
+  }
+
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        GestureDetector(
+            child:
+            Icon(icon, color: color),
+            onTap: () {
+              // do what you need to do when "Click here" gets clicked
+              launch('https://es.wikipedia.org/wiki/Regi%C3%B3n_Huetar_Atl%C3%A1ntica');
+            }
+        ),
+
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Center(
+            child:
+            GestureDetector(
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: color,
+                  ),
+                ),
+                onTap: () {
+                  // do what you need to do when "Click here" gets clicked
+                  launch('https://es.wikipedia.org/wiki/Regi%C3%B3n_Huetar_Atl%C3%A1ntica');
+                }
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+}
+
+/// Sample linear data type.
+class LinearSales {
+  final int year;
+  final double sales;
+
+  LinearSales(this.year, this.sales);
+}
